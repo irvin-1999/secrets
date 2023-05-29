5d44ff850b6b0f2e69720872646c6b5236025f65
Boitumelo_Iven Moshitwa <itumelo553@gmail.com>
2023-05-29 17:15:51 +0200

    add security to the app, level 1(user password and email) and level 2 of encryption of the password

--- app.js
+++ app.js
@@ -1,19 +1,62 @@
 const express = require("express")
 const ejs = require("ejs")
 const bodyParser = require('body-parser')
+const monngoose = require('mongoose')
+const encrypt = require('mongoose-encryption')
+const { default: mongoose } = require("mongoose")
 
-const app = express
+const app = express()
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 
+monngoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true })
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const secret = "Thisisourlittlesecret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+const User = new mongoose.model("user", userSchema)
+
 app.get('/', (req, res) => {
     res.render("home")
 })
 
+app.get('/login', (req, res) => {
+    res.render("login")
+})
+
+app.get('/register', (req, res) => {
+    res.render("register")
+})
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save().then(res.render('secrets')).catch((err) => { console.error(err) })
+})
 
 
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password
+
+    User.findOne({ email: username }).then((foundUser) => {
+        if (foundUser) {
+            if (foundUser.password === password) {
+                res.render('secrets')
+            }
+        }
+    }).catch((err) => { console.error(err) })
+})
 
 
 app.listen(3000, () => {
