8448c30c1cab774f7fc499c672d24fda44bde598
Boitumelo_Iven Moshitwa <itumelo553@gmail.com>
2023-05-29 11:57:32 +0200

    add server start codes

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-//jshint esversion:6
+const express = require("express")
+const ejs = require("ejs")
+const bodyParser = require('body-parser')
+
+const app = express
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set("view engine", "ejs")
+
+app.get('/', (req, res) => {
+    res.render("home")
+})
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("App is listenning on Port 3000...")
+})
